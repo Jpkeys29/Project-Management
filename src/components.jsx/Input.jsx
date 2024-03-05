a947d53cfc36baa660a8f9492f142675991bfbff
Jp Keys <jpmusik29@gmail.com>
2024-03-05 09:07:21 -0500

    Displaying bug fixed

--- src/components.jsx/Input.jsx
+++ src/components.jsx/Input.jsx
@@ -1,6 +1,6 @@
 export default function Input({label, textarea, ...props}) {
     return <p>
-        <label></label>
-        {textarea ? <textarea {...props}/> : <input {...props}/>}
+        <label>{label}</label>
+        {textarea ? <textarea {...props} /> : <input {...props} />}
     </p>
 }
