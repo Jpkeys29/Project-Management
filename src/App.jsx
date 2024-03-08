53e2d9e8c1fc52c81d4245472da325d484283703
Jp Keys <jpmusik29@gmail.com>
2024-03-08 09:02:32 -0500

    Setting up State for Add-project

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
+import { useState } from "react";
 import SideBar from "./components.jsx/Sidebar";
 import NewProject from "./components.jsx/NewProject";
 import NoProjectSelected from "./components.jsx/NoProjectSelected";
 
 function App() {
+  const[projectState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  })
+
+  
+
   return (
     <main className="container">
       <h1>Project Manager</h1>
